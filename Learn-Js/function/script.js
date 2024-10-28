7829dd3a2f7f36fa8789446d0789afcde88d756a
Pratham-Makwana <makwanapratham13@gmail.com>
2024-10-28 13:13:33 +0530

    Function In JS

---
+++ Learn-Js/function/script.js
@@ -0,0 +1,29 @@
+function sunOfNumber(a,b){
+    const sum = a + b;
+    console.log("result is ", sum);
+    return sum;
+}
+
+const a = 12;
+const b = 4;
+const result = sunOfNumber(a, b);
+console.log("result is ",result);
+
+
+const c= 2;
+const d = 24;
+sunOfNumber(c,d);
+
+const e = 5;
+const f = 14;
+sunOfNumber(e,f);
+
+const square = function (num){
+    return num * num;
+}
+
+
+console.log(square);
+
+console.log(square(3));
+
